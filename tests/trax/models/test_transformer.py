0cf23460a1836862c5df8fe94b7891d7cd9b6856
CookieBox26 <34176970+CookieBox26@users.noreply.github.com>
2022-01-15 22:55:41 +0900

    add trax

---
+++ tests/trax/models/test_transformer.py
@@ -0,0 +1,15 @@
+import trax
+
+class TestTraxModelsTransformer:
+
+    def test_init(self):
+        model = trax.models.Transformer(
+            input_vocab_size=33300,
+            d_model=512,
+            d_ff=2048,
+            n_heads=8,
+            n_encoder_layers=6,
+            n_decoder_layers=6,
+            max_len=2048,
+            mode='predict'
+        )
